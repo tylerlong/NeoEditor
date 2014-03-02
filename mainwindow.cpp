e0ffc37f2eead6f7c1f45d47028b7a5db013cd4d
Tyler Long <tyler4long@gmail.com>
2014-03-02 20:06:06 +0800

    Use splitter instead of layout

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,6 +5,7 @@ MainWindow::MainWindow(QApplication *app)
     QDesktopWidget *desktop = app->desktop();
     this->resizeAndCenter(desktop->width(), desktop->height());
 
+    //left panel
     QTreeView *treeView1 = new QTreeView(this);
     QFileSystemModel *fileSystemModel = new QFileSystemModel(this);
     fileSystemModel->setRootPath(QDir::homePath());
@@ -14,28 +15,21 @@ MainWindow::MainWindow(QApplication *app)
     treeView1->setColumnHidden(2, true);
     treeView1->setColumnHidden(3, true);
     treeView1->setHeaderHidden(true);
-
     QTreeView *treeView2 = new QTreeView(this);
-
     QToolBox *toolBox = new QToolBox(this);
     toolBox->addItem(treeView1, QDir::homePath());
     toolBox->addItem(treeView2, tr("test2"));
 
+    //right panel
     QWebView *webView = new QWebView(this);
     webView->load(QUrl("qrc:///html/index.html"));
     webView->page()->mainFrame()->addToJavaScriptWindowObject("mainWindow", this);
     webView->page()->mainFrame()->addToJavaScriptWindowObject("app", app);
 
-    QHBoxLayout *layout = new QHBoxLayout();
-    QVBoxLayout *leftLayout = new QVBoxLayout();
-    QVBoxLayout *rightLayout = new QVBoxLayout();
-    leftLayout->addWidget(toolBox);
-    rightLayout->addWidget(webView);
-    layout->addLayout(leftLayout);
-    layout->addLayout(rightLayout);
-    QWidget *centralWidget = new QWidget(this);
-    this->setCentralWidget(centralWidget);
-    centralWidget->setLayout(layout);
+    QSplitter *splitter = new QSplitter(Qt::Horizontal);
+    splitter->addWidget(toolBox);
+    splitter->addWidget(webView);
+    this->setCentralWidget(splitter);
 }
 
 void MainWindow::resizeAndCenter(int screenWidth, int screenHeight)
