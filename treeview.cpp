cbe9339ed5ad9aa22a17fd4aea83cb8a1be9944e
Tyler Long <tyler4long@gmail.com>
2014-03-16 01:22:10 +0800

    Add new file context menu

--- treeview.cpp
+++ treeview.cpp
@@ -13,8 +13,14 @@ void TreeView::showContextMenu(const QPoint &point)
     {
         return;
     }
+
     QFileSystemModel *fileSystemModel = (QFileSystemModel*)this->model();
     QFileInfo fileInfo = fileSystemModel->fileInfo(currentIndex);
+    if(!(fileInfo.isFile()||fileInfo.isDir()))
+    {
+        return;
+    }
+
     if(fileInfo.isFile())
     {
         QMenu menu(this);
@@ -23,9 +29,34 @@ void TreeView::showContextMenu(const QPoint &point)
         menu.addAction(&deleteFileAction);
         menu.exec(this->mapToGlobal(point));
     }
+    else if(fileInfo.isDir())
+    {
+        QMenu menu(this);
+        QAction newFileAction(tr("&New File"), this);
+        connect(&newFileAction, SIGNAL(triggered()), this, SLOT(newFile()));
+        menu.addAction(&newFileAction);
+        menu.exec(this->mapToGlobal(point));
+    }
 }
 
 void TreeView::deleteFile()
 {
+    QFileSystemModel *fileSystemModel = (QFileSystemModel*)this->model();
+    QFileInfo fileInfo = fileSystemModel->fileInfo(this->currentIndex());
+    if(!fileInfo.isFile())
+    {
+        return;
+    }
     qDebug() << "delete file";
+}
+
+void TreeView::newFile()
+{
+    QFileSystemModel *fileSystemModel = (QFileSystemModel*)this->model();
+    QFileInfo fileInfo = fileSystemModel->fileInfo(this->currentIndex());
+    if(!fileInfo.isDir())
+    {
+        return;
+    }
+    qDebug() << "new file";
 }
--- treeview.h
+++ treeview.h
@@ -14,6 +14,7 @@ public:
 private slots:
     void showContextMenu(const QPoint &point);
     void deleteFile();
+    void newFile();
 };
 
 
